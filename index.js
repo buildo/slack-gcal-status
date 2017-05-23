0bf3f2c1f6bf7573d22a3c6b3fe0720898bb57f2
Gabriele Petronella <gabriele@buildo.io>
2017-05-23 15:10:35 +0200

    Parse endTime. Fixes #1

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const { slackToken, calendar } = require('./config.json')
 
 function profileForMeeting(meeting) {
   if (meeting) {
-    const [date, time, title] = meeting.split('  ');
+    const [startDate, startTime, endDate, endTime, title] = meeting.split('\t');
     return encodeURIComponent(JSON.stringify({
-      "status_text": `In a meeting ('${title}') which ends at${time}`,
+      "status_text": `In a meeting ('${title}') which ends at ${endTime}`,
       "status_emoji": ":calendar:"
     }));
   } else {
@@ -19,7 +19,7 @@ function profileForMeeting(meeting) {
 }
 
 const agendaCommand = `
-  gcalcli --calendar ${calendar} agenda --nocolor $(date +%Y-%m-%dT%H:%M:%S) $(date -v +1M +%Y-%m-%dT%H:%M:%S)
+  gcalcli --calendar ${calendar} agenda --nocolor --tsv $(date +%Y-%m-%dT%H:%M:%S) $(date -v +1M +%Y-%m-%dT%H:%M:%S)
 `;
 const agenda = execSync(agendaCommand).toString();
 const currentMeeting = agenda.split('\n').filter(x => x.trim() !== '')[0]
@@ -28,5 +28,5 @@ const endpoint = `
   https://slack.com/api/users.profile.set?token=${slackToken}&profile=${profile}&pretty=1
 `;
 
-console.log(`Setting status to ${JSON.stringify(currentMeeting)}`);
+console.log(`Setting status to ${JSON.stringify(currentMeeting.replace(/\t/, ' '))}`);
 axios.get(endpoint).catch(console.log.bind(console));
