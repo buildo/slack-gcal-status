7e20e517d3b8ecbaaabc579fafc9e25a7a63f17a
Gabriele Petronella <gabriele@buildo.io>
2017-05-30 16:54:47 +0200

    Add Docker support

--- README.md
+++ README.md
@@ -11,21 +11,22 @@ It clears the status if you're not in a meeting.
 - [gcalcli](https://github.com/insanum/gcalcli)
   - install with `pip install gcalcli`
   - run `gcalcli agenda` to configure its authentication
-  
+
 ## Installation
 - clone the repo
 - run `yarn` (or `npm install`) to install the dependencies
 - create a file `config.json` with these two keys:
 
   ```json
-  {
+  [{
     "slackToken": "xosometoken-123123",
     "calendar": "myname@buildo.io
+  }]
   ```
-  
+
    - You can get a Slack token from https://api.slack.com/custom-integrations/legacy-tokens.
    - `calendar` is the name of your calendar (usually `yourname@buildo.io`)
-      
+
 - run `node index.js` to update the status
 
 ## Continuously update
--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const execSync = require('child_process').execSync;
 const axios = require('axios');
+const moment = require('moment');
 
-const { slackToken, calendar } = require('./config.json')
+const configurations = require('./config.json');
 
 function profileForMeeting(meeting) {
   if (meeting) {
@@ -18,20 +19,27 @@ function profileForMeeting(meeting) {
   }
 }
 
-const agendaCommand = `
-  gcalcli --calendar ${calendar} agenda --nocolor --tsv $(date +%Y-%m-%dT%H:%M:%S) $(date -v +1M +%Y-%m-%dT%H:%M:%S)
-`;
-const agenda = execSync(agendaCommand).toString();
-const currentMeeting = agenda.split('\n').filter(x => x.trim() !== '')[0]
-const profile = profileForMeeting(currentMeeting);
-const endpoint = `
-  https://slack.com/api/users.profile.set?token=${slackToken}&profile=${profile}&pretty=1
-`;
+function updateStatus({ slackToken, calendar }) {
+  const now = moment().format();
+  const nowPlus1m = moment().add(1, 'minute').format();
 
-if (currentMeeting) {
-  console.log(`Setting status to ${JSON.stringify(currentMeeting.replace(/\t/, ' '))}`);
-} else {
-  console.log('Clearing the current status');
+  const agendaCommand = `
+    gcalcli --calendar ${calendar} agenda --nocolor --tsv ${now} ${nowPlus1m}
+  `;
+  const agenda = execSync(agendaCommand).toString();
+  const currentMeeting = agenda.split('\n').filter(x => x.trim() !== '')[0]
+  const profile = profileForMeeting(currentMeeting);
+  const endpoint = `
+    https://slack.com/api/users.profile.set?token=${slackToken}&profile=${profile}&pretty=1
+  `;
+
+  if (currentMeeting) {
+    console.log(`Setting status to ${JSON.stringify(currentMeeting.replace(/\t/, ' '))}`);
+  } else {
+    console.log('Clearing the current status');
+  }
+
+  return axios.get(endpoint).catch(console.log.bind(console));
 }
 
-axios.get(endpoint).catch(console.log.bind(console));
+configurations.forEach(updateStatus);
